46cefc67103afc527b08f3a8d3f3cca4560e38f3
mohammadloo_meysam <mohammadloo.developer@gmail.com>
2022-02-21 15:57:52 +0330

    change all file

--- jav.js
+++ jav.js
@@ -4,50 +4,53 @@ var i;
 for(i = 0;i<myNodeList.length;i++){
     var span = document.createElement('SPAN')
     var txt = document.createTextNode('\u00d7');
-    span.className='close'
+    span.className ='close'
     span.appendChild(txt);
-    myNodeList[i].appendChild(span)
+    myNodeList[i].appendChild(span);
 }
+
 var closeb = document.getElementsByClassName('close')
 var i;
-for(i=0; i<closeb.length; i++){
+for(i=0; i< closeb.length; i++){
+    
     closeb[i].onclick=function(){
-        var div = this.parentElement;
-        div.style.display='noun'
+        var divv = this.parentElement;
+        divv.style.display='none'
     }
 }
 var list = document.querySelector('ul')
-list.addEventListener('click',function(ev){
-    if(ev.target.tagname ==='LI'){
-        ev.target.classlist.toggle('checked')
+list.addEventListener('click' ,function(ev){
+    if(ev.target.tagName ==='LI'){
+        ev.target.classList.toggle('checked')
     }
 },false)
-
 
 function newElement(){
     var li = document.createElement('li')
     var inputValue = document.getElementById('item').value
     var t = document.createTextNode(inputValue)
-    li.appendChild(t)
+    li.appendChild(t);
     if(inputValue === ""){
-        alert('this field can nit be empty')
+        alert('this field can nit be empty');
     }else{
         document.getElementById('list').appendChild(li)
 
     }
-    document.getElementById('item').value=''
+    document.getElementById('item').value='';
 
     var span = document.createElement('SPAN');
-    var txt = document.createTextNode('/u00d7')
-    span.appendChild(txt)
-    li.appendChild(span)
+    var txt = document.createTextNode('/u00d7');
+    span.appendChild(txt);
+    li.appendChild(span);
 
-    for(i=0; i <closeb.length;i++){
+    for(i=0; i< closeb.length; i++){
+    
         closeb[i].onclick=function(){
-            var div = this.parentElement;
-            div.style.display='noun'
+            var divv = this.parentElement;
+            divv.style.display='none'
         }
     }
 
 
 }
+
